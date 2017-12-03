f90666cbf54f6e2fa37b74f3dba0e74cf24ad82a
szreich18 <szreich18@brandeis.edu>
2017-12-03 15:09:08 -0500

    Added Print Line

--- GroupProject.java
+++ GroupProject.java
@@ -1,5 +1,6 @@
 public class GroupProject {
   public static void main(String[] args) {
     System.out.println("Welcome to the fantasy draft!!!!");
+    System.out.println("I'm just here so I wont get fined");
   }
 }
