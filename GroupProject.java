71272a0b674cb8698c1711ee8ebbf42ea7f279a2
szreich18 <szreich18@brandeis.edu>
2017-12-03 15:14:39 -0500

    Reads in the CSV file

--- GroupProject.java
+++ GroupProject.java
@@ -1,6 +1,19 @@
 public class GroupProject {
   public static void main(String[] args) {
+
+      /* Reads the csv file
+        */
+      TextIO.readFile("Fs.csv");
+      TextIO.getln();  // skip the line of headers...
+      int pos=0;
+      while (!TextIO.eof()){
+        String line = TextIO.getln(); // read in the next line from the file
+
+        String[] fields = line.split(","); // split the columns apart
+
+
     System.out.println("Welcome to the fantasy draft!!!!");
-    System.out.println("I'm just here so I wont get fined");
+
+
   }
 }
