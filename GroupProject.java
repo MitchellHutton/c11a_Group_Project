7e766d4ad73414c47ddc671597c889ec920463f1
MitchellHutton <calmitch@brandeis.edu>
2017-12-03 15:48:15 -0500

    variable changes

--- GroupProject.java
+++ GroupProject.java
@@ -1,17 +1,17 @@
 public class GroupProject {
   public static void main(String[] args) {
 
-      int dbSize = 600;
+      int csvLength = 500;
 
-      String[] player = new String[dbSize];    // col 0
-      String[] position = new String[dbSize];  // col 1
-      String[] team = new String[dbSize];      // col 2
+      String[] player = new String[csvLength];    // col 0
+      String[] position = new String[csvLength];  // col 1
+      String[] team = new String[csvLength];      // col 2
 
-      int[] rebounds = new int[dbSize];        // col 3
-      int[] assists = new int[dbSize];         // col 4
-      int[] steals = new int[dbSize];          // col 5
-      int[] blocks = new int[dbSize];          // col 6
-      int[] points = new int[dbSize];          // col 7
+      int[] rebounds = new int[csvLength];        // col 3
+      int[] assists = new int[csvLength];         // col 4
+      int[] steals = new int[csvLength];          // col 5
+      int[] blocks = new int[csvLength];          // col 6
+      int[] points = new int[csvLength];          // col 7
 
       // Read csv
       TextIO.readFile("fs.csv");
@@ -20,24 +20,27 @@ public class GroupProject {
 
       while (!TextIO.eof()) {
 
-        String line = TextIO.getln(); // read in the next line from the file
-        String[] fields = line.split(","); // split the columns apart
+        String cell = TextIO.getln(); // read in the next cell from the file
+        String[] columns = cell.split(","); // split the columns apart
 
-      //establish categories
-      player[cate] = fields[0];
-      position[cate] = fields[1];
-      team[cate] = fields[2];
-      rebounds[cate] = Integer.parseInt(fields[3]);
-      assists[cate] = Integer.parseInt(fields[4]);
-      steals[cate] = Integer.parseInt(fields[5]);
-      blocks[cate] = Integer.parseInt(fields[6]);
-      points[cate] = Integer.parseInt(fields[7]);
+      //establish categories (cate)
+      player[cate] = columns[0];
+      position[cate] = columns[1];
+      team[cate] = columns[2];
+      rebounds[cate] = Integer.parseInt(columns[3]);
+      assists[cate] = Integer.parseInt(columns[4]);
+      steals[cate] = Integer.parseInt(columns[5]);
+      blocks[cate] = Integer.parseInt(columns[6]);
+      points[cate] = Integer.parseInt(columns[7]);
 
       cate++;
 
       } // end of while loop
 
       System.out.println("Welcome to the fantasy draft!!!!");
+      System.out.println("This is a 4 team NBA fantasy draft.");
+      System.out.println("The stats are Points, Steals, Blocks, Assists, and Rebounds.");
+      System.out.println("Good luck!");
 
   } // end of main
 
